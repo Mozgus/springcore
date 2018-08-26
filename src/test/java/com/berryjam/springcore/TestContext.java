4cb7538fb303b3eba61a714b82b3ef333b9fc5b5
Mozgus <retailnet@rambler.ru>
2018-08-26 19:07:06 +0300

    #8 It is even more context opportunities

---
+++ src/test/java/com/berryjam/springcore/TestContext.java
@@ -0,0 +1,21 @@
+package com.berryjam.springcore;
+
+import static org.junit.Assert.assertEquals;
+
+import com.berryjam.springcore.beans.Client;
+import org.junit.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestContext {
+    @Test
+    public void testPropertyPlaceholderSystemOverride() {
+        System.setProperty("client.id", "35");
+        ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+        Client client = ctx.getBean(Client.class);
+        ctx.close();
+
+        assertEquals("35", client.getId());
+    }
+
+}
